9931a4451701d13a079d37c1a34568014526ba1c
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2026-03-23 10:55:11 +0100

    Run Pint

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -19,19 +19,19 @@ class UserSeeder extends Seeder
                 'name' => 'Beheerder Rob',
                 'email' => 'beheerder@roca12.nl',
                 'password' => Hash::make('B3he3rd3r@r0ca!2'),
-                'role' => 'admin'
+                'role' => 'admin',
             ],
             [
                 'name' => 'Docent Jan',
                 'email' => 'docent@roca12.nl',
                 'password' => Hash::make('D0c3nt@r0ca!2'),
-                'role' => 'teacher'
+                'role' => 'teacher',
             ],
             [
                 'name' => 'Student Frenkie',
                 'email' => 'student@st.roc.a12.nl',
                 'password' => Hash::make('Stud3nt@r0ca!'),
-                'role' => 'student'
+                'role' => 'student',
             ],
         ]);
     }
